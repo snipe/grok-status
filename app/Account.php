8877c13fb18a31194405c290dec87b2c3e1e53ed
snipe <snipe@snipe.net>
2017-11-29 04:05:42 -0800

    Add some basic relationships

--- app/Account.php
+++ app/Account.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    //
+    public function owner()
+    {
+        return $this->belongsTo('\App\User', 'owner_id');
+    }
+
+    public function incidents()
+    {
+        return $this->hasMany('\App\Incident', 'account_id');
+    }
+
+    public function setHomeUrlAttribute($key, $value)
+    {
+
+    }
+
+
 }
