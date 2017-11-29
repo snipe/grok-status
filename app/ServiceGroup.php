3834b71420242cd21728c6d8c211dad543f9b6a7
snipe <snipe@snipe.net>
2017-11-29 04:07:57 -0800

    Added some basic model methods and global query scopes

--- app/ServiceGroup.php
+++ app/ServiceGroup.php
@@ -3,8 +3,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\MultiTenantScope;
+use App\Scopes\ActiveScope;
 
 class ServiceGroup extends Model
 {
-    //
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new MultiTenantScope);
+        static::addGlobalScope(new ActiveScope);
+    }
+
+    public function incidents()
+    {
+        return $this->hasMany('\App\Incident', 'servicegroup_id');
+    }
+
+    public function services()
+    {
+        return $this->hasMany('\App\Service', 'servicegroup_id');
+    }
+
 }
--- app/Subscription.php
+++ app/Subscription.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\MultiTenantScope;
 
 class Subscription extends Model
 {
