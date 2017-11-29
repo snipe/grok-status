6dbd5231befe8a2b63c837b65ad05391f75d5e9a
snipe <snipe@snipe.net>
2017-11-29 04:09:22 -0800

    Fixed welcome page to use app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,113 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
+
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>
+        @if (isset($current_account))
+            {{ $current_account->company_name }} Status Page
+        @else
+            {{ config('app.name') }}
+        @endif
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    </title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/clean-blog.css" rel="stylesheet">
+
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            @if (isset($current_account))
+                {{ $current_account->company_name }} Status Page (Account # {{ $current_account->id }} )
+            @else
+                {{ config('app.name') }}
+            @endif
+        </a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+            @if (Route::has('login'))
+                <ul class="navbar-nav ml-auto">
+                    @auth
+                    <li class="nav-item"><a href="{{ url('/manage') }}">Manage</a></li>
+                    @else
+                        <li class="nav-item"><a href="{{ route('login') }}">Login</a></li>
+                        <li class="nav-item"><a href="{{ route('register') }}">Register</a></li>
+                        @endauth
+                </ul>
+            @endif
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+        </div>
+    </div>
+</nav>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+<!-- Page Header -->
+<header class="masthead" style="height: 70px;">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10" style="height: 70px;">
             </div>
-        </nav>
+        </div>
+    </div>
+</header>
+
+<!-- Main Content -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
 
         @yield('content')
     </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+
+
+    </div>
+</div>
+</div>
+
+<hr>
+
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <p class="copyright text-muted">Copyright &copy; {{ config('app.name') }} {{ date('Y') }}</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+
+<!-- Custom scripts for this template -->
+<script src="js/clean-blog.min.js"></script>
+
 </body>
-</html>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,132 +1,64 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+@extends('layouts.app')
 
-<head>
+@section('content')
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+            <!-- service groups -->
+            @if (isset($servicegroups))
+                @foreach ($servicegroups as $servicegroup)
 
-    <title>
-        @if (isset($account))
-            {{ $account->company_name }} Status Page
-        @else
-            {{ config('app.name') }}
-        @endif
+                    <div class="post-preview">
 
-    </title>
+                            <h4 class="post-subtitle">
+                                {{ $servicegroup->name }}
+                            </h4>
+                            <p class="post-meta">{{ $servicegroup->description }}</p>
 
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+                            @if ($servicegroup->incidents->count() > 0)
+                                @foreach ($servicegroup->incidents as $incident)
 
-    <!-- Custom fonts for this template -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+                                    <div class="alert alert alert-danger">
+                                        <i class="fa fa-exclamation-circle"></i>
+                                        <strong>{{ $incident->name }} </strong><br>
+                                        {{ $incident->description }}
+                                    </div>
 
-    <!-- Custom styles for this template -->
-    <link href="css/clean-blog.min.css" rel="stylesheet">
+                                @endforeach
 
-</head>
+                            @endif
 
-<body>
 
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand" href="{{ config('app.url') }}">
-            @if (isset($account))
-                {{ $account->company_name }} Status Page
+                    </div>
+                    <!-- services -->
+                    @if ($servicegroup->services->count() > 0)
+                        <table class="table table-striped">
+                        @foreach ($servicegroup->services as $service)
+
+                            <tr>
+                                <td>
+                                    {{ $service->name }}
+                                </td>
+                                <td>
+                                    @if ($service->url!='')
+                                        <a href="{{ $service->url }}">{{ $service->url }}</a>
+                                    @endif
+                                </td>
+                                <td>
+                                    {{ $service->description }}
+                                </td>
+
+
+                            </tr>
+
+                        @endforeach
+                        </table>
+                    @endif
+
+                @endforeach
+                    <hr>
             @else
-                {{ config('app.name') }}
-            @endif
-        </a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fa fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-
-            @if (Route::has('login'))
-                <ul class="navbar-nav ml-auto">
-                    @auth
-                    <li class="nav-item"><a href="{{ url('/home') }}">Home</a></li>
-                    @else
-                        <li class="nav-item"><a href="{{ route('login') }}">Login</a></li>
-                        <li class="nav-item"><a href="{{ route('register') }}">Register</a></li>
-                        @endauth
-                </ul>
+                <h3 class="post-subtitle">
+                    No status information is available yet!
+                </h3>
             @endif
 
-        </div>
-    </div>
-</nav>
-
-<!-- Page Header -->
-<header class="masthead" style="height: 70px;">
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-md-10" style="height: 70px;">
-            </div>
-        </div>
-    </div>
-</header>
-
-<!-- Main Content -->
-<div class="container">
-    <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-
-            <div class="post-preview">
-                <a href="post.html">
-                    <h2 class="post-title">
-                        Failure is not an option
-                    </h2>
-                    <h3 class="post-subtitle">
-                        Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-                    </h3>
-                </a>
-                <p class="post-meta">Posted by
-                    <a href="#">Start Bootstrap</a>
-                    on July 8, 2017</p>
-            </div>
-            @if (isset($account))
-                THIS IS A VALID ACCOUNT
-            @else
-                NO ACCOUNT INFO - SHOW DEFAULT HOMEPAGE
-            @endif
-
-            <hr>
-            <!-- Pager -->
-            <div class="clearfix">
-                <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-            </div>
-        </div>
-    </div>
-</div>
-
-<hr>
-
-<!-- Footer -->
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-                <p class="copyright text-muted">Copyright &copy; Your Website 2017</p>
-            </div>
-        </div>
-    </div>
-</footer>
-
-<!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- Custom scripts for this template -->
-<script src="js/clean-blog.min.js"></script>
-
-</body>
-
-</html>
+@endsection
