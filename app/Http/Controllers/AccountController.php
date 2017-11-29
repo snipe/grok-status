72f41ceb70ad391b81d8f55d07ad3b9311be0aca
snipe <snipe@snipe.net>
2017-11-29 04:06:05 -0800

    Derp.

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -7,10 +7,6 @@ use Illuminate\Http\Request;
 class AccountController extends Controller
 {
     
-    public function owner()
-    {
-        return $this->belongsTo('\App\User', 'owner_id');
-    }
-    
-    
+
+
 }
